1f63fb01530823d4d01020ea89e555a0b0b88974
Andrew Dunham <andrew@du.nham.ca>
2016-01-27 01:40:15 -0800

    More protocols, add tests

--- src/detect.rs
+++ src/detect.rs
@@ -9,6 +9,8 @@ type DetectFn = fn(&[u8]) -> DetectResult;
 
 const PROTOCOLS: &'static [(&'static str, DetectFn)] = &[
     ("tls", detect_is_tls),
+    ("http", detect_is_http),
+    ("ssh", detect_is_ssh),
 ];
 
 
@@ -27,6 +29,48 @@ fn detect_is_tls(buf: &[u8]) -> DetectResult {
     }
 }
 
+
+fn detect_is_ssh(buf: &[u8]) -> DetectResult {
+    if buf.len() < 4 {
+        return DetectResult::NotEnoughData;
+    }
+
+    if buf.starts_with(b"SSH-") {
+        DetectResult::Success
+    } else {
+        DetectResult::Failure
+    }
+}
+
+fn detect_is_http(buf: &[u8]) -> DetectResult {
+    if buf.len() < 3 {
+        return DetectResult::NotEnoughData;
+    }
+
+    // Fast path: search the buffer for 'HTTP'
+    if buf.windows(4).any(|b| b == b"HTTP") {
+        return DetectResult::Success;
+    }
+
+    let methods: &[&'static [u8]] = &[
+        b"GET",
+        b"PUT",
+        b"HEAD",
+        b"POST",
+        b"TRACE",
+        b"DELETE",
+        b"CONNECT",
+        b"OPTIONS"
+    ];
+    for method in methods.iter() {
+        if buf.windows(method.len()).any(|b| b == *method) {
+            return DetectResult::Success;
+        }
+    }
+
+    DetectResult::Failure
+}
+
 pub fn detect(buf: &[u8]) -> Option<&'static str> {
     for &(name, ff) in PROTOCOLS.iter() {
         if ff(buf) == DetectResult::Success {
@@ -41,4 +85,27 @@ pub fn protocol_names() -> Vec<&'static str> {
     PROTOCOLS.iter()
         .map(|&(name, _)| name)
         .collect::<Vec<_>>()
+}
+
+
+#[test]
+fn test_detect_is_ssh() {
+    assert_eq!(detect_is_ssh(b"notssh"), DetectResult::Failure);
+    assert_eq!(detect_is_ssh(b"SSH-1.2"), DetectResult::Success);
+    assert_eq!(detect_is_ssh(b"aa"), DetectResult::NotEnoughData);
+}
+
+#[test]
+fn test_detect_is_http() {
+    assert_eq!(detect_is_http(b"nothttp"), DetectResult::Failure);
+    assert_eq!(detect_is_http(b"HTTP/1.0 200 OK"), DetectResult::Success);
+    assert_eq!(detect_is_http(b"GET /"), DetectResult::Success);
+}
+
+#[test]
+fn test_detect_is_tls() {
+    // First bits of a ClientHello header
+    assert_eq!(detect_is_tls(b"\x16\x03\x02\x00\x31\x01\x00\x00\x2d\x03\x02"), DetectResult::Success);
+    assert_eq!(detect_is_tls(b"other data"), DetectResult::Failure);
+    assert_eq!(detect_is_tls(b"aa"), DetectResult::NotEnoughData);
 }
